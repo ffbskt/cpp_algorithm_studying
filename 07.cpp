c7e8ae268eab55f803d7aa6d51cf919bd917c1dc
denis <ffbskt@gmail.com>
2018-07-10 20:35:23 +0300

    my_new general folder of studing tasks

---
+++ 07.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+#define forn(i, n) for(int i = 0; i < n; ++i)
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    
+    return 0;
+}
